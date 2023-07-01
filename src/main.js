6f935ec2dce9a6be0f9e4a1284c2b7838aad95c7
Jack White <whjjackwhite@gmail.com>
2023-07-01 17:27:06 +0530

    First Completion

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 class SectController {
-    constructor(sections) {
+    constructor(sections, doc) {
         this.sections = sections;
         this.activeSection = undefined;
     }
@@ -25,32 +25,67 @@ class SectController {
 }
 
 class PageController {
-    constructor(sectController) {
+    constructor(sectController, doc) {
         this.sectController = sectController;
+        this.pagerContainer = doc;
+        this.pageItems = [];
+        this.createPager();
+    }
+
+    createPager() {
+        const pager = document.createElement("pager");
+        const itemContainer = document.createElement("itemcontainer");
+        pager.appendChild(itemContainer);
+        for (var i = 0; i < this.sectController.sections.length; i++) {
+            var pageItem = document.createElement("item");
+            pageItem.id = "pager" + i;
+            const n = i;
+            pageItem.onclick = () => {
+                this.pageAt(n);
+            }
+            this.pageItems.push(pageItem);
+            itemContainer.appendChild(pageItem);
+        }
+        this.pagerContainer.appendChild(pager);
+    }
+
+    updatePager() {
+        for (let i = 0; i < this.pageItems.length; i++) {
+            if (i == this.sectController.active) {
+                this.pageItems[i].classList.add("active");
+            } else {
+                this.pageItems[i].classList.remove("active");
+            }
+        }
     }
 
     nextPage() {
-        sections.active = (sections.active >= (sections.sections.length - 1)) ? sections.active : (sections.active + 1);
+        this.sectController.active = (this.sectController.active >= (this.sectController.sections.length - 1)) ? this.sectController.active : (sections.active + 1);
+        this.updatePager();
     }
     prevPage() {
-        sections.active = (sections.active <= 0) ? sections.active : (sections.active - 1);
+        this.sectController.active = (this.sectController.active <= 0) ? this.sectController.active : (this.sectController.active - 1);
+        this.updatePager();
     }
 
     pageAt(index) {
-        sections.active = index;
+        this.sectController.active = index;
+        this.updatePager();
     }
 }
 
+var documentElement = document.querySelector("html");
+
 var sections = new SectController(document.querySelectorAll(".section"));
-var pageContrl = new PageController(sections);
+var pageContrl = new PageController(sections, documentElement);
 
-var navTouchElem = document.querySelector("html");
-var navTouchRegion = new ZingTouch.Region(navTouchElem);
+var navTouchRegion = new ZingTouch.Region(documentElement);
 
-navTouchRegion.bind(navTouchElem, 'swipe', function(e) {
-    if (e.currentDirection in Range(10, 170)) {
+navTouchRegion.bind(documentElement, 'swipe', function(e) {
+    console.log(e);
+    if (e.detail.data[0].currentDirection > 10 && e.detail.data[0].currentDirection < 170) {
         pageContrl.nextPage();
-    } else if (e.currentDirection in Range(190, 350)) {
+    } else if (e.detail.data[0].currentDirection > 190 && e.detail.data[0].currentDirection < 360) {
         pageContrl.prevPage();
     }
 });
